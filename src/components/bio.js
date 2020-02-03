486b0b150e71faff50fa10a04a3a13c2e3d982de
Quinn Shanahan <q.shanahan@gmail.com>
2020-02-02 19:23:51 -0500

    webp idk

--- src/components/bio.js
+++ src/components/bio.js
@@ -17,7 +17,7 @@ const Bio = () => {
       avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
         childImageSharp {
           fixed(width: 50, height: 50, quality: 90) {
-            ...GatsbyImageSharpFixed_withWebp
+            ...GatsbyImageSharpFixed
           }
         }
       }
