c8494f1e383200469fe9107ab638af78add6bd70
Quinn Shanahan <q.shanahan@gmail.com>
2020-02-08 15:11:58 -0500

    looks like there was already a feed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,3 @@
-const feedSerializer = ({ query: { site, allMarkdownRemark } }) => {
-	return allMarkdownRemark.edges.map(edge => {
-		const siteUrl = site.siteMetadata.siteUrl
-		const postText = `
-		<div style="margin-top=55px; font-style: italic;">(This is an article posted to my blog at quinn.io. You can read it online by <a href="${siteUrl +
-			edge.node.fields.slug}">clicking here</a>.)</div>
-	`
-
-		let html = edge.node.html
-		// Hacky workaround for https://github.com/gaearon/overreacted.io/issues/65
-		html = html
-			.replace(/href="\//g, `href="${siteUrl}/`)
-			.replace(/src="\//g, `src="${siteUrl}/`)
-			.replace(/"\/static\//g, `"${siteUrl}/static/`)
-			.replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
-
-		return Object.assign({}, edge.node.frontmatter, {
-			description: edge.node.frontmatter.description,
-			date: edge.node.frontmatter.date,
-			url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-			guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-			custom_elements: [{ 'content:encoded': html + postText }],
-		})
-	})
-}
-
 module.exports = {
 	siteMetadata: {
 		title: `Quinn's Blog`,
