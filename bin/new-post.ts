e1c4ee2149d79e35a36c3753e35db66e648f565d
Quinn Shanahan <q.shanahan@gmail.com>
2022-05-23 21:51:14 -0400

    create script for adding new post

---
+++ bin/new-post.ts
@@ -0,0 +1,28 @@
+import os from 'os'
+import fs from 'fs'
+import yaml from 'yaml'
+
+function zpad (number: Number) {
+    return ('00' + number).slice(-2)
+}
+
+const title = process.argv[2]
+const date = new Date()
+
+const titleSlug = title.replace(new RegExp(' ', "g"), '-').toLowerCase()
+const slug = `${zpad(date.getMonth())}-${zpad(date.getDate())}-${titleSlug}`
+const path = `content/blog/${date.getFullYear()}/${slug}`
+const filename = `${path}/index.md`
+
+if (!fs.existsSync(path)) {
+    fs.mkdirSync(path)
+}
+
+const tpl = yaml.stringify({
+    title,
+    date: date.toISOString(),
+})
+
+fs.writeFileSync(filename, `---\n${tpl}---\n`)
+
+console.log(`Created ${filename}`)
